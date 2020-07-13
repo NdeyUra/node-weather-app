1ca7d5cfcb7faef42400111039b87b84699ef0b7
NdyUra <NdyUra@gmail.com>
2020-07-13 11:30:58 +0530

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('javaScript loaded successfully');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.getElementById('message-1')
